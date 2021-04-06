6f21f8858d0bca2c77757d9513590cce8cb968c1
Eugen Kozenkov <klon19892@gmail.com>
2021-04-06 12:11:05 +0300

    docs: update js-files and index.html

--- hw_1/variables.js
+++ hw_1/variables.js
@@ -6,19 +6,19 @@ var userInfo;*/
 
 //part 2
 console.log(test);
-var test = 'string'; // undefined
+var test = 'string'; // undefined, объявление прошло, но вывлеи до присвоения значения 
 
 var x = 'string';
 var x = 'string 2';
-console.log(x); // string 2
+console.log(x); // string 2, перезаписали переменную
 
 console.log(test2);
-let test2 = 'string'; // error
+let test2 = 'string'; // error, объявили после вывода
 
 const x2 = 'string';
 const x2 = 'string 2';
-console.log(x2); // error
+console.log(x2); // error, нельзя константе присвоить новое значение
 
 let num = 12;
 let num = 1;
-console.log(num); // error
+console.log(num); // error, повторное объявление переменной
